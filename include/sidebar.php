482d4aeee7ba9b5c50827b4b21717d2069c9eb60
ikshakil <iksakil2@gmail.com>
2024-07-03 10:34:42 +0600

    update

--- include/sidebar.php
+++ include/sidebar.php
@@ -4,56 +4,27 @@
         <div class="quixnav">
             <div class="quixnav-scroll">
                 <ul class="metismenu" id="menu">
-                    <li class="nav-label first">Main Menu</li>
-                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
-                                class="icon icon-single-04"></i><span class="nav-text">Dashboard</span></a>
-                        <ul aria-expanded="false">
-                            <li><a href="<?= $baseurl ?>assets/index.html">Dashboard 1</a></li>
-                            <li><a href="<?= $baseurl ?>assets/index2.html">Dashboard 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
-                                class="icon icon-chart-bar-33"></i><span class="nav-text">Management</span></a>
-                        <ul aria-expanded="false">
-                            <li><a href="javascript:void()">Class</a>
-                                <ul aria-expanded="false">
-                                <li><a href="<?= $baseurl ?>class_add.php"> Add</a>
-                                <li><a href="<?= $baseurl ?>class_list.php"> List</a>
-                                </ul>
-                            </li>
-                            <li><a href="javascript:void()">Section</a>
-                                <ul aria-expanded="false">
-                                    <li><a href="<?= $baseurl ?>section_add.php"> Add</a>
-                                    <li><a href="<?= $baseurl ?>section_list.php"> List</a>
-                                </ul>
-                            </li>
-                            <li><a href="javascript:void()">Group</a>
-                                <ul aria-expanded="false">
-                                    <li><a href="<?= $baseurl ?>group_add.php"> Add</a>
-                                    <li><a href="<?= $baseurl ?>group_list.php"> List</a>
-                                </ul>
-                            </li>
-                            <li><a href="javascript:void()">Subject</a>
-                                <ul aria-expanded="false">
-                                    <li><a href="<?= $baseurl ?>subject_add.php"> Add</a>
-                                    <li><a href="<?= $baseurl ?>subject_list.php"> List</a>
-                                </ul>
-                            </li>
-                            <li><a href="javascript:void()">Session</a>
-                                <ul aria-expanded="false">
-                                    <li><a href="<?= $baseurl ?>session_add.php"> Add</a>
-                                    <li><a href="<?= $baseurl ?>session_list.php"> List</a>
-                                </ul>
-                            </li>
-                          
-                        </ul>
-                    </li>
-                    <li class="nav-label">Components</li>
+                    <li class="nav-label first">Student Menu</li>
                     <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
                                         class="icon icon-single-04"></i><span class="nav-text">Student</span></a>
                         <ul aria-expanded="false">
                             <li><a href="<?= $baseurl ?>student_add.php">Add</a></li>
                             <li><a href="<?= $baseurl ?>student_list.php">List</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                        class="icon icon-form"></i><span class="nav-text">Student Attendance</span></a>
+                        <ul aria-expanded="false">
+                            <li><a href="<?= $baseurl ?>student_attendance_add.php">Add</a></li>
+                            <li><a href="<?= $baseurl ?>student_attendance_list.php">List</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-label">Marks</li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                                        class="icon icon-layout-25"></i><span class="nav-text">Subject Marks</span></a>
+                        <ul aria-expanded="false">
+                            <li><a href="<?= $baseurl ?>class_subject_add.php">Add</a></li>
+                            <li><a href="<?= $baseurl ?>class_subject_list.php">List</a></li>
                         </ul>
                     </li>
                     <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
@@ -63,6 +34,40 @@
                             <li><a href="<?= $baseurl ?>student_marks_list.php">List</a></li>
                         </ul>
                     </li>
+                    <li class="nav-label first">Class Managment</li>
+                       
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                            class="icon icon-form"></i><span class="nav-text">Class</span></a>
+                                <ul aria-expanded="false">
+                                <li><a href="<?= $baseurl ?>class_add.php"> Add</a>
+                                <li><a href="<?= $baseurl ?>class_list.php"> List</a>
+                                </ul>
+                            </li>
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                            class="icon icon-form"></i><span class="nav-text">Section</span></a>
+                                <ul aria-expanded="false">
+                                    <li><a href="<?= $baseurl ?>section_add.php"> Add</a>
+                                    <li><a href="<?= $baseurl ?>section_list.php"> List</a>
+                                </ul>
+                            </li>
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                        class="icon icon-form"></i><span class="nav-text">Group</span></a>
+                                <ul aria-expanded="false">
+                                    <li><a href="<?= $baseurl ?>group_add.php"> Add</a>
+                                    <li><a href="<?= $baseurl ?>group_list.php"> List</a>
+                                </ul>
+                            </li> 
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                        class="icon icon-form"></i><span class="nav-text">Session</span></a>
+                                <ul aria-expanded="false">
+                                    <li><a href="<?= $baseurl ?>session_add.php"> Add</a>
+                                    <li><a href="<?= $baseurl ?>session_list.php"> List</a>
+                                </ul>
+                            </li> 
+                    </li>
+                    <li class="nav-label">Teacher Manu</li>
+                    
+                   
                     <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
                                 class="icon icon-single-04"></i><span class="nav-text">Teacher</span></a>
                         <ul aria-expanded="false">
@@ -71,16 +76,11 @@
                             <li><a href="<?= $baseurl ?>teacher_list.php">List</a></li>
                         </ul>
                     </li>
-
                     <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
-                                class="icon icon-single-04"></i><span class="nav-text">Teacher Information</span></a>
+                        class="icon icon-form"></i><span class="nav-text">Teacher Attendance</span></a>
                         <ul aria-expanded="false">
-                            <li><a href="<?= $baseurl ?>department_add.php">Department</a></li>
-                            <li><a href="<?= $baseurl ?>department_list.php">Department List</a></li>
-                        </ul>
-                        <ul aria-expanded="false">
-                            <li><a href="<?= $baseurl ?>designation_add.php">Designation</a></li>
-                            <li><a href="<?= $baseurl ?>designation_list.php">Designation List</a></li>
+                            <li><a href="<?= $baseurl ?>teacher_attendance_add.php">Add</a></li>
+                            <li><a href="<?= $baseurl ?>teacher_attendance_list.php">List</a></li>
                         </ul>
                     </li>
                     <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
@@ -92,36 +92,20 @@
                         </ul>
                     </li>
                     <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
-                                class="icon icon-single-04"></i><span class="nav-text">Routine Components</span></a>
-                        <ul aria-expanded="false">
-                            <li><a href="<?= $baseurl ?>day_name_add.php">Day Name</a></li>
-                            <li><a href="<?= $baseurl ?>day_name_list.php">Day List</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-label">Attendance</li>
-                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
-                        class="icon icon-form"></i><span class="nav-text">Student Attendance</span></a>
-                        <ul aria-expanded="false">
-                            <li><a href="<?= $baseurl ?>student_attendance_add.php">Add</a></li>
-                            <li><a href="<?= $baseurl ?>student_attendance_list.php">List</a></li>
-                        </ul>
-                    </li>
-
-                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
-                        class="icon icon-form"></i><span class="nav-text">Teacher Attendance</span></a>
-                        <ul aria-expanded="false">
-                            <li><a href="<?= $baseurl ?>teacher_attendance_add.php">Add</a></li>
-                            <li><a href="<?= $baseurl ?>teacher_attendance_list.php">List</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-label">Subject</li>
-                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
-                                        class="icon icon-layout-25"></i><span class="nav-text">Class Subject</span></a>
-                        <ul aria-expanded="false">
-                            <li><a href="<?= $baseurl ?>class_subject_add.php">Add</a></li>
-                            <li><a href="<?= $baseurl ?>class_subject_list.php">List</a></li>
-                        </ul>
-                    </li>
+                            class="icon icon-form"></i><span class="nav-text">Department</span></a>
+                                <ul aria-expanded="false">
+                                    <li><a href="<?= $baseurl ?>department_add.php"> Add</a>
+                                    <li><a href="<?= $baseurl ?>department_list.php"> List</a>
+                                </ul>
+                            </li>
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                            class="icon icon-form"></i><span class="nav-text">Designation</span></a>
+                                <ul aria-expanded="false">
+                                    <li><a href="<?= $baseurl ?>designation_add.php"> Add</a>
+                                    <li><a href="<?= $baseurl ?>designation_list.php"> List</a>
+                                </ul>
+                            </li>
+                    
                     <li class="nav-label">Fees</li>
                     <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
                                 class="icon icon-layout-25"></i><span class="nav-text">Fees Category</span></a>
