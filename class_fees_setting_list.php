1ed82a8bf4c813195cfa7be8005bb8dc952aa277
Suraiya444 <suraiya444444@gmail.com>
2024-07-07 02:13:42 +0600

    class-fees

--- class_fees_setting_list.php
+++ class_fees_setting_list.php
@@ -75,76 +75,52 @@
                                 </div>
                             </form>
                         </div>
-<?php 
-    //  $period=$day_name=$class_routine=[];
-    //  $result=$mysqli->common_select('period');
-    //  if($result){
-    //      if($result['data']){
-    //          foreach($result['data'] as $d){
-    //              $period[$d->sl]=$d;
-    //          }
-    //      }
-    //  }
-     $result=$mysqli->common_select('fees_category');
-     if($result){
-         if($result['data']){
-             foreach($result['data'] as $d){
-                 $name[$d->id]=$d;
-             }
-         }
-     }
-    if(isset($_GET['class_id']) && $_GET['group_id'] && $_GET['session_id']){
-        $result=$mysqli->common_select_query("SELECT class_fees_setting.*, fees_category.name FROM `class_fees_setting` 
-                                        JOIN fees_category on fees_category.id=class_fees_setting.fees_id                                 
-                                        where class_fees_setting.class_id={$_GET['class_id']} and class_fees_setting.group_id={$_GET['group_id']} and class_fees_setting.session_id={$_GET['session_id']}");
-    }    
-?>
 
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered verticle-middle table-responsive-sm">
-                                    <thead>
-                                        <tr> 
-                                            <th scope="col">ID</th>
-                                            <th scope="col">Fees</th>
-                                            <th scope="col">Amount</th>
-                                            <th scope="col">Actions</th>
-                                           
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php if($name){
-                                                foreach($name as $d){
-                                        ?>
-                                            <tr>
-                                                <th><?= $d->id?></th>
-                                                <?php if($name){
-                                                        foreach($name as $p){
-                                                ?>
-                                                    <td>
-                                                        <?php 
-                                                            if(isset($fees_category[$p->name])){
-                                                                echo $fees_category[$d->sl][$p->sl]->amount;
-                                                                
-                                                            }
-                                                        ?>
-                                                    </td>
-                                                <?php } } ?>
-                                                <td>
-                                                <span>
-                                                    <a href="<?= $baseurl ?>class_fees_setting_edit.php?id=<?= $data ->id ?>" class="mr-4" data-toggle="tooltip"
-                                                        data-placement="top" title="Edit"><i
-                                                            class="fa fa-pencil color-muted"></i> </a>
-                                                    <a href="<?= $baseurl ?>class_fees_setting_delete.php?id=<?= $data ->id ?>" data-toggle="tooltip"
-                                                        data-placement="top" title="Close"><i
-                                                            class="fa fa-close color-danger"></i></a>
-                                                </span>
-                                            </td>
-                                            </tr>
-                                        <?php } } ?>
-                                    </tbody> 
-                                </table>
-                            </div>
+
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered verticle-middle table-responsive-sm">
+                    <thead>
+                        <tr> 
+                            <th scope="col">ID</th>
+                            <th scope="col">Fees</th>
+                            <th scope="col">Amount</th>
+                            <th scope="col">Actions</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php 
+    
+            if(isset($_GET['class_id']) && $_GET['group_id'] && $_GET['session_id']){
+            $result=$mysqli->common_select_query("SELECT class_fees_setting.*,fees_category.name ,fees_category.id FROM `class_fees_setting` 
+            JOIN fees_category on fees_category.fees_id=class_fees_setting.fees_id                                 
+          where class_fees_setting.class_id={$_GET['class_id']} 
+          and class_fees_setting.group_id={$_GET['group_id']}
+            and class_fees_setting.session_id={$_GET['session_id']} ");
+                        if($result){
+                            if($result['data']){
+                                foreach($result['data'] as $data){ ?>
+            <tr>
+            <td><?= $data->id ?></td>
+            <td><?= $data-> name?></td>
+            <td><?= $data-> amount?></td>
+            <td>
+                <span>
+                    <a href="<?= $baseurl ?>class_fees_setting_edit.php?id=<?= $data ->id ?>" class="mr-4" data-toggle="tooltip"
+                        data-placement="top" title="Edit"><i
+                            class="fa fa-pencil color-muted"></i> </a>
+                    <a href="<?= $baseurl ?>class_fees_setting_delete.php?id=<?= $data ->id ?>" data-toggle="tooltip"
+                        data-placement="top" title="Close"><i
+                            class="fa fa-close color-danger"></i></a>
+                </span>
+            </td>
+        </tr>
+        <?php } } } } ?>
+
+                    </tbody> 
+                </table>
+            </div>
                         </div>
                     </div>
                 </div>
